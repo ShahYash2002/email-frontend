35d5c4b46f5d9f8c94982f08b1fed0cd7aa3ce00
Yash Shah <yshah@argusoft.com>
2023-03-23 11:19:35 +0530

    api request changes

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://email-backend-api.onrender.com/",
   withCredentials: true,
+  headers: {
+    Origin: "https://my-mailbox.netlify.app/",
+  },
 });
 
 // api.interceptors.request.use((config) => {
