86dc0a5c5e10196270762a688c06fd43a928e425
Yash Shah <yshah@argusoft.com>
2023-03-23 10:42:45 +0530

    api request changes

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://email-backend-api.onrender.com/",
+  withCredentials: true,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.token;
-  config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.token;
+//   config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
 
 export default api;
